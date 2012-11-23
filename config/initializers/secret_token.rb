64fb92419ba5db7c0cf87172d5cc987efd9295b6
Matthew Powers <matthewkevinpowers@gmail.com>
2012-11-22 21:00:10 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuSpike::Application.config.secret_token = 'f2179b8bc9a96032536f59fcf657d3ca24d13c574d6fc96803e67a696632f89d4dad9329e4b78236b503e01e4c1645e61266e8fbb984fb7859361151c6f09557'
